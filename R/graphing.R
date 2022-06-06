41f481a81ebb518de8119aed0f92e3cf359bab0f
gafnts <gafnts@gmail.com>
2022-06-06 13:34:33 -0600

    theme_gravity()

--- R/graphing.R
+++ R/graphing.R
@@ -19,3 +19,31 @@ you_want_it_darker <- function(...) {
     plot.margin = unit(c(.5, .5, .4, .2), "cm")
   )
 }
+
+# Gravity
+
+theme_gravity <- function(...) {
+  theme(
+    text = element_text(color = "gray15",
+                        family = "Helvetica Neue",
+                        size = 19),
+    # Axis
+    axis.title = element_blank(),
+    axis.text = element_text(color = "gray15"),
+    axis.ticks = element_blank(),
+    # Legend
+    legend.title = element_blank(),
+    legend.position = c(0.81, .92),
+    legend.direction = "horizontal",
+    legend.key = element_rect(fill = "white"),
+    legend.background = element_rect(color = "gray15", size = 0.25),
+    # Panel
+    panel.background = element_rect(color = "gray15", fill = "white"),
+    panel.grid = element_blank(),
+    # Facets
+    strip.background = element_rect(color = "gray15", fill = "gray15"),
+    strip.text = element_text(color = "white"),
+    # Margins
+    plot.margin = unit(c(.5,0.1,.5,0.1), "cm")
+  )
+}
